75fb5e7729186ebacc83f1a619d0f81b7b356acc
Larry Xing <nextgenxing@gmail.com>
2021-01-25 11:55:49 -0800

    attacking

--- Jamin_Bot/engine/Evaluate.h
+++ Jamin_Bot/engine/Evaluate.h
@@ -62,7 +62,7 @@ double eval(state s)
 	score -= dpawn_coeff * ((double)doubled_white - doubled_black);
 	for(int i = 0; i < 8; i++)
 		for(int j = 0; j < 8; j++)
-			score += activity_coeff*s.board[i][j]*(s.attacking(i, j, s.board[i][j] < 0)-s.attacking(i, j, s.board[i][j] >= 0));
+			score += attacking_coeff*s.board[i][j]*(s.attacking(i, j, s.board[i][j] < 0)-s.attacking(i, j, s.board[i][j] >= 0));
 	return score;
 }
 #endif // !EVALUATE_H_INCLUDED
