63b69960c933ef5cadc1c919fdab92131715ac6f
David <jeffarjeffar@gmail.com>
2021-01-26 06:54:02 -0800

    stuff

--- Jamin_Bot/Jamin_Bot.py
+++ Jamin_Bot/Jamin_Bot.py
@@ -5,6 +5,15 @@ from discord.ext import commands
 from cogs.Misc import *
 from cogs.Beat_Jamin import *
 
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='debug.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+
 bot = commands.Bot(command_prefix='$')
 
 bot.add_cog(Beat_Jamin(bot))
