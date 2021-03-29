a06bbdfcce6e574b745cb8587ebc9f2886c7bd92
jeffarjeffar <jeffarjeffar@gmail.com>
2021-03-29 10:42:51 -0700

    fix run engine

--- Chess_Bot/cogs/CPP_IO.py
+++ Chess_Bot/cogs/CPP_IO.py
@@ -37,7 +37,7 @@ def prepare_input(person, move=''):
 
 async def run_engine(file_in, file_out):
 	# print('Running engine')
-	out, err, status = await util.run(f'.\\engine < {file_in} > {file_out}')
+	out, err, status = await util.run(f'./engine < {file_in} > {file_out}')
 	# print(f'Stdout: {out}\nStderr: {err}\n{status}')
 
 
